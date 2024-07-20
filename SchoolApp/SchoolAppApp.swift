6dc5989d6e26dd34b3ae32cf91717dd2d2c9b19c
esse.jacques.co <essedansomon@gmail.com>
2024-07-20 14:27:07 +0000

    Initial Commit

---
+++ SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by Esse Jacques  on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
